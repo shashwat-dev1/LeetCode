524fae8451891b1348290b3e9730d50253fcb69d
Shashwat Jain <114853741+shashwat-dev1@users.noreply.github.com>
2026-04-09 01:07:34 +0530

    Time: 1861 ms (5.08%), Space: 20 MB (95.77%) - LeetHub

---
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def xorAfterQueries(self, nums, queries):
+        mod = 1000000007
+        for t in queries:
+            l = t[0]
+            r = t[1]
+            k = t[2]
+            v = t[3]
+            idx = l
+            while idx <= r:
+                temp = nums[idx]
+                nums[idx] = (temp * v) % mod
+                idx += k
+        ans = 0
+        for num in nums:
+            ans ^= num
+
+        return ans
